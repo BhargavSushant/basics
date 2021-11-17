36d1a5aa40fe546635948abd75e8e61f851ee7ea
BhargavSushant <bhargavsushant@gmail.com>
2021-11-17 16:18:14 -0500

    first commit

---
+++ helloword.js
@@ -0,0 +1,28 @@
+// What is node
+// RTE = ( JS  + V8 engine)*C language=> Server
+// PROTOTYPING => Agile Dev => Fast and Highly Scalable => JS Everywhere
+// Cleaner and more consistent codebase
+// Open Source libs
+
+/**
+ * How node Works
+ * > Asyncronous : analogy : like a waiter kitchen chef customer analogy
+ * > this is called non blocking
+ * > single thread , event loop, callback, promise, event queue
+ * > Node apps are async by default,
+ * > Must not be used for CPU intensive applications. (Video processing, Data Crunching , ML that sort of stuff)
+ */
+
+/**INSTALL NODE
+ * 
+ */
+
+/** set up node environment */
+
+function fun(){
+    return 42;
+}
+
+console.log(fun()); fun();
+
+  
---
+++ nodevjs.js
@@ -0,0 +1,8 @@
+// process.argv
+// global.console.log
+/*BROWSER:      WINDOW   DOCUMENT    HISTORY LOCATION    NAVIGATOR*/
+/*NODE   :      global    process     module  __filename   require()*/
+
+/**BROWSER : URL */
+/**NODE     : URL class by whatwg ( Web Hypertext Application Technology Working Group=2004 group of engineers
+ * from Apple Mozilla Opera  ) this group sets standards in HTML */
